9b53e66078bc63382f365693c3e939a692da3871
Stephen Burgess <stephen.burgess@chime.com>
2024-10-14 21:23:10 -0700

    Added code comments

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const LOCAL_STORAGE_SKIP_QUESTIONS = 'skip-questions';
 
 (function () {
+	/**
+	 * Adjust colors depending on the time of the day
+	 */
+	if (isDayTimeHours()) {
+		document.body.classList.add('bg-white');
+		document.body.classList.add('text-slate-700');
+		document.getElementById('footer').classList.add('bg-gray-100');
+		document.getElementById('reset').classList.add('bg-gray-300');
+	}
+
+	/**
+	 * Get the questions filtered by the ones you know
+	 */
 	const skipQuestions = getFromLocalStorage(LOCAL_STORAGE_SKIP_QUESTIONS) || [];
 	const chosenOne = getQuestion(skipQuestions);
-
-	console.log('chosenOne:', chosenOne);
 
 	if (chosenOne?.number) {
 		document.getElementById('question').textContent = chosenOne.question;
@@ -15,10 +26,9 @@ const LOCAL_STORAGE_SKIP_QUESTIONS = 'skip-questions';
 		document.getElementById('you-are-prepared').classList.remove('hidden');
 	}
 
-	window.addEventListener('beforeunload', function () {
-		window.scrollTo(0, 0);
-	});
-
+	/**
+	 * Handler for when correct 👍 button pressed so we can throw the Q in skip questions array & refresh
+	 */
 	const correctBtn = document.getElementById('correct');
 	correctBtn.addEventListener('click', function () {
 		saveToLocalStorage(LOCAL_STORAGE_SKIP_QUESTIONS, [...skipQuestions, chosenOne.number]);
@@ -30,6 +40,9 @@ const LOCAL_STORAGE_SKIP_QUESTIONS = 'skip-questions';
 		}, 300);
 	});
 
+	/**
+	 * Handler for the reset button so we can clear skip Qs array and refresh
+	 */
 	const resetBtn = document.getElementById('reset');
 	resetBtn.addEventListener('click', function () {
 		saveToLocalStorage(LOCAL_STORAGE_SKIP_QUESTIONS, []);
@@ -38,19 +51,22 @@ const LOCAL_STORAGE_SKIP_QUESTIONS = 'skip-questions';
 		}, 1);
 	});
 
+	/**
+	 * Once DOM is loaded full we can set the info text
+	 */
 	document.addEventListener(
 		'DOMContentLoaded',
 		function () {
 			const percentCorrect = Math.round((skipQuestions.length / chosenOne.total) * 100);
-			document.getElementById('info').textContent = `${percentCorrect}%`;
+			document.getElementById('info').textContent = `You know ${percentCorrect}% of ${chosenOne.total} Qs`;
 		},
 		false,
 	);
 
-	if (isDayTimeHours()) {
-		document.body.classList.add('bg-white');
-		document.body.classList.add('text-slate-700');
-		document.getElementById('footer').classList.add('bg-gray-100');
-		document.getElementById('reset').classList.add('bg-gray-300');
-	}
+	/**
+	 * When page reloads always scroll to the top to prevent seeing answers of next question
+	 */
+	window.addEventListener('beforeunload', function () {
+		window.scrollTo(0, 0);
+	});
 })();
